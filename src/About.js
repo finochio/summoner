653569ed24eca998dd5630527696cb3d94c03e99
finochio <51974895+finochio@users.noreply.github.com>
2019-10-26 00:55:32 -0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ import dockerkube from './assets/dockerkube.jpg'
 export const About = () => (
   <Fragment className = 'about'>
       <p><strong> </strong>&nbsp;&nbsp;<a href="http://finochio.mynetgear.com">Link to: Application Demo</a></p>
-      <p><strong> </strong>&nbsp;&nbsp;<a href="https://github.com/finochio/highspot-coding-exercise">Link to: Documentation and Code</a></p>
+      <p><strong> </strong>&nbsp;&nbsp;<a href="https://github.com/finochio/summoner">Link to: Documentation and Code</a></p>
       <a href={dockerkube}><img border="0" alt="Docker Hub Build + Kubernetes Dashboards" src={dockerkube} width="768"></img></a>
   </Fragment>
 )
